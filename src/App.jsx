3693bf0c9b457e0690bb56e0004f295a0f815633
Sanket Singh <singhsanket143@gmail.com>
2024-12-01 10:35:25 +0530

    Added basic signup card for auth page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 
-import { Button } from '@/components/ui/button';
+import { Route, Routes } from 'react-router-dom';
+
+import { Auth } from './pages/Auth/Auth';
 function App() {
 
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,14 @@
+import { SignupCard } from '@/components/organisms/Auth/SignupCard';
+
+export const Auth = () => {
+    return (
+        <div 
+            className="h-[100vh] flex items-center justify-center bg-[#5c3B58]"
+        >
+            <div className="md:h-auto md:w-[420px]">
+                <SignupCard />
+            </div>
+
+        </div>
+    );
+};
