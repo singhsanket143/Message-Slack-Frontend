33dfd4a660e77fc381d3cda4c03d0470ba700fe6
Sanket Singh <singhsanket143@gmail.com>
2024-12-01 11:23:07 +0530

    Added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { Route, Routes } from 'react-router-dom';
 import { SigninCard } from '@/components/organisms/Auth/SigninCard';
 import { SignupCard } from '@/components/organisms/Auth/SignupCard';
 import { Auth } from '@/pages/Auth/Auth';
+import { Notfound } from '@/pages/Notfound/Notfound';
 function App() {
 
   return (
     <Routes>
       <Route path="/auth/signup" element={<Auth><SignupCard /></Auth>} />
       <Route path="/auth/signin" element={<Auth><SigninCard /></Auth>} />
+
+      <Route path="/*" element={<Notfound />} />
     </Routes>
   );
 }
