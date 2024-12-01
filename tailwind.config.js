9ca86d9ba1c71c59cac4d1e494a42450b3677fde
Sanket Singh <singhsanket143@gmail.com>
2024-12-01 10:39:29 +0530

    Added custom slack color to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
+			slack: {
+				DEFAULT: '#5c3B58'
+			},
   			card: {
   				DEFAULT: 'hsl(var(--card))',
   				foreground: 'hsl(var(--card-foreground))'
