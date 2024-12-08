0ba3e0261bbf4703666054aeaa2c515d6bd23615
Sanket Singh <singhsanket143@gmail.com>
2024-12-08 12:32:34 +0530

    Added get Workspace by id api

--- src/apis/workspaces/index.js
+++ src/apis/workspaces/index.js
@@ -32,3 +32,17 @@ export const fetchWorkspacesRequest = async ({ token }) => {
         throw error.response.data;
     }
 };
+
+export const fetchWorkspaceDetailsRequest = async ({ workspaceId, token }) => {
+    try {
+        const response = await axios.get(`/workspaces/${workspaceId}`, {
+            headers: {
+                'x-access-token': token
+            }
+        });
+        return response?.data?.data;
+    } catch(error) {
+        console.log('Error in fetching workspace details request', error);
+        throw error.response.data;
+    }
+};
