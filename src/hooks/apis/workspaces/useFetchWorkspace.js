95178712e08b1e89f966007845884a9f45e11a4b
Sanket Singh <singhsanket143@gmail.com>
2024-12-08 10:49:45 +0530

    Consumed useFetchWorkspaces

--- src/hooks/apis/workspaces/useFetchWorkspace.js
+++ src/hooks/apis/workspaces/useFetchWorkspace.js
@@ -8,7 +8,7 @@ export const useFetchWorkspace = () => {
 
     const { isFetching, isSuccess, error, data: workspaces } = useQuery({
         queryFn: () => fetchWorkspacesRequest({ token: auth?.token }),
-        queryKey: 'fetchWorkspaces',
+        queryKey: ['fetchWorkspaces'],
         staleTime: 30000
     });
 
