ebd35cea83495eec47c7a6041bfb75e7060aac8a
Sanket Singh <singhsanket143@gmail.com>
2024-12-08 10:20:27 +0530

    Added logout

--- src/components/atoms/UserButton/UserButton.jsx
+++ src/components/atoms/UserButton/UserButton.jsx
@@ -1,12 +1,27 @@
 import { LogOutIcon, SettingsIcon } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { useAuth } from '@/hooks/context/useAuth';
+import { useToast } from '@/hooks/use-toast';
 
 export const UserButton = () => {
 
-    const { auth } = useAuth();
+    const navigate = useNavigate();
+
+    const { auth, logout } = useAuth();
+    const { toast } = useToast();
+
+    async function handleLogout() {
+        await logout();
+        toast({
+            title: 'Successfully signed out',
+            type: 'success'
+        });
+        navigate('/auth/signin');
+
+    }
 
     return (
         <DropdownMenu>
@@ -21,7 +36,7 @@ export const UserButton = () => {
                     <SettingsIcon className='size-4 mr-2 h-10' />
                     Settings
                 </DropdownMenuItem>
-                <DropdownMenuItem>
+                <DropdownMenuItem onClick={handleLogout}>
                     <LogOutIcon className='size-4 mr-2 h-10' />
                     Logout
                 </DropdownMenuItem>
