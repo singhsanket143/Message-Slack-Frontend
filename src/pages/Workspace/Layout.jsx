5016cefa0fdf3bdfb82b367092420a68179732c3
Sanket Singh <singhsanket143@gmail.com>
2024-12-14 11:46:02 +0530

    Added resiable sidebar for building channel list

--- src/pages/Workspace/Layout.jsx
+++ src/pages/Workspace/Layout.jsx
@@ -1,5 +1,6 @@
 import { WorkspaceNavbar } from '@/components/organisms/Workspace/WorkspaceNavbar';
 import { WorkspaceSidebar } from '@/components/organisms/Workspace/WorkspaceSidebar';
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 
 export const WorkspaceLayout = ({ children }) => {
     return (
@@ -7,7 +8,24 @@ export const WorkspaceLayout = ({ children }) => {
             <WorkspaceNavbar />
             <div className="flex h-[calc(100vh-40px)]">
                 <WorkspaceSidebar />
-                {children}
+                <ResizablePanelGroup direction="horizontal" autoSaveId={'workspace-resize'}>
+                    <ResizablePanel
+                        defaultSize={20}
+                        minSize={11}
+                        className='bg-slack-medium'
+                    >
+                        <div>
+                            Sidebar
+                        </div>
+                    </ResizablePanel>
+                    <ResizableHandle withHandle/>
+                    <ResizablePanel
+                        minSize={20}
+                    >
+                        {children}
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+                
             </div>
         </div>
     );
