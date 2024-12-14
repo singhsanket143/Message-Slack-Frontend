cbc61f57260a1ebcafe007c0eaccaef8344e0711
Sanket Singh <singhsanket143@gmail.com>
2024-12-14 10:36:58 +0530

    Added more workspace ui

--- src/components/organisms/Workspace/WorkspaceSidebar.jsx
+++ src/components/organisms/Workspace/WorkspaceSidebar.jsx
@@ -2,12 +2,14 @@ import { BellIcon, HomeIcon, MessageSquareIcon, MoreHorizontalIcon } from 'lucid
 
 import { UserButton } from '@/components/atoms/UserButton/UserButton';
 import { SidebarButton } from '@/components/molecules/SidebarButton/SidebarButton';
+import { WorkspaceSwitcher } from '@/components/organisms/Workspace/WorkspaceSwitcher';
 
 export const WorkspaceSidebar = () => {
     return (
         <aside
             className="w-[70px] h-full bg-slack-dark flex flex-col gap-y-4 items-center pt-[10px] pb-[5px]"
         >
+            <WorkspaceSwitcher />
 
             <SidebarButton 
                 Icon={HomeIcon}
