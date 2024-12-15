ad87aabb2f85f92ac37cb8e8c684e19688d7e51c
Sanket Singh <singhsanket143@gmail.com>
2024-12-15 12:13:37 +0530

    Implemented update workspace

--- src/components/molecules/CreateWorkspaceModal/CreateWorkspaceModal.jsx
+++ src/components/molecules/CreateWorkspaceModal/CreateWorkspaceModal.jsx
@@ -1,3 +1,4 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,6 +9,9 @@ import { useCreateWorkspace } from '@/hooks/apis/workspaces/useCreateWorkspace';
 import { useCreateWorkspaceModal } from '@/hooks/context/useCreateWorkspaceModal';
 
 export const CreateWorkspaceModal = () => {
+
+    const queryClient = useQueryClient();
+
     const { openCreateWorkspaceModal, setOpenCreateWorkspaceModal } = useCreateWorkspaceModal();
 
     const { isPending, createWorkspaceMutation } = useCreateWorkspace();
@@ -26,6 +30,8 @@ export const CreateWorkspaceModal = () => {
             const data = await createWorkspaceMutation({ name: workspaceName });
             console.log('Created the workspace', data);
             navigate(`/workspaces/${data._id}`);
+            queryClient.invalidateQueries('fetchWorkspaces');
+
         } catch(error) {
             console.log('Not able to create a new workspace', error);
         } finally {
--- src/components/molecules/Workspace/WorkspacePreferencesModal.jsx
+++ src/components/molecules/Workspace/WorkspacePreferencesModal.jsx
@@ -3,8 +3,11 @@ import { TrashIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
 import { useDeleteWorkspace } from '@/hooks/apis/workspaces/useDeleteWorkspace';
+import { useUpdateWorkspace } from '@/hooks/apis/workspaces/useUpdateWorkspace';
 import { useWorkspacePreferencesModal } from '@/hooks/context/useWorkspacePreferencesModal';
 import { useToast } from '@/hooks/use-toast';
 
@@ -14,10 +17,14 @@ export const WorkspacePreferencesModal = () => {
     const navigate = useNavigate();
     const { toast } = useToast();
     const [workspaceId, setWorkspaceId] = useState(null);
+    const [editOpen, setEditOpen] = useState(false);
 
 
     const { initialValue, openPreferences, setOpenPreferences, workspace } = useWorkspacePreferencesModal();
     const { deleteWorkspaceMutation } = useDeleteWorkspace(workspaceId);
+    const { isPending, updateWorkspaceMutation } = useUpdateWorkspace(workspaceId);
+
+    const [renameValue, setRenameValue] = useState(workspace?.name);
 
     function handleClose() {
         setOpenPreferences(false);
@@ -25,6 +32,7 @@ export const WorkspacePreferencesModal = () => {
 
     useEffect(() => {
         setWorkspaceId(workspace?._id);
+        setRenameValue(workspace?.name);
     }, [workspace]);
 
     async function handleDelete() {
@@ -46,6 +54,25 @@ export const WorkspacePreferencesModal = () => {
         }
     }
 
+    async function handleFormSubmit(e) {
+        e.preventDefault();
+        try {
+            await updateWorkspaceMutation(renameValue);
+            queryClient.invalidateQueries(`fetchWorkspaceById-${workspace?._id}`);
+            setOpenPreferences(false);
+            toast({
+                title: 'Workspace updated successfully',
+                type: 'success'
+            });
+        } catch(error) {
+            console.log('Error in updating workspace', error);
+            toast({
+                title: 'Error in updating workspace',
+                type: 'error'
+            });
+        }
+    }
+
     return (
         <Dialog open={openPreferences} onOpenChange={handleClose}>
             <DialogContent>
@@ -56,32 +83,73 @@ export const WorkspacePreferencesModal = () => {
                 </DialogHeader>
 
                 <div className='px-4 pb-4 flex flex-col gap-y-2'>
-                    <div
-                        className='px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50'
-                    >   
-                        <div className='flex items-center justify-between'>
-                            <p
-                                className='font-semibold text-sm'
-                            >
-                                Workspace Name
-                            </p>
-                            <p
-                                className='text-sm font-semibold hover:underline'
-                            >
-                                Edit
-                            </p>
-                        </div>
 
-                        <p
-                            className='text-sm'
-                        >
-                            {initialValue}
-                        </p>
+                    <Dialog open={editOpen} onOpenChange={setEditOpen}>
+                        <DialogTrigger>
+                                <div
+                                    className='px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50'
+                                >   
+                                    <div className='flex items-center justify-between'>
+                                        <p
+                                            className='font-semibold text-sm'
+                                        >
+                                            Workspace Name
+                                        </p>
+                                        <p
+                                            className='text-sm font-semibold hover:underline'
+                                        >
+                                            Edit
+                                        </p>
+                                    </div>
 
+                                    <p
+                                        className='text-sm'
+                                    >
+                                        {initialValue}
+                                    </p>
+                             </div>
 
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>
+                                    Rename Workspace
+                                </DialogTitle>
+                            </DialogHeader>
 
-                    </div>
+                            <form className='space-y-4' onSubmit={handleFormSubmit}>
+                                <Input
+                                    value={renameValue}
+                                    onChange={(e) => setRenameValue(e.target.value)}
+                                    required
+                                    autoFocus
+                                    minLength={3}
+                                    maxLength={50}
+                                    disabled={isPending}
+                                    placeholder='Workspace Name e.g. Design Team'
+                                />
+                            
 
+                                <DialogFooter>
+                                    <DialogClose>
+                                        <Button
+                                            variant="outline"
+                                            disabled={isPending}
+                                        >
+                                            Cancel
+                                        </Button>
+                                    </DialogClose>
+                                    <Button
+                                        type="submit"
+                                        disabled={isPending}
+                                    >
+                                        Save
+                                    </Button>
+                                </DialogFooter>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
+                    
                     <button
                         className='flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50'
                         onClick={handleDelete}
