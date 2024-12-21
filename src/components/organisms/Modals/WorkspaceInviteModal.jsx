6d42bff3600bf122915fc9c9b12c0b5ee910fcee
Sanket Singh <singhsanket143@gmail.com>
2024-12-21 11:47:52 +0530

    Added join page logic

--- src/components/organisms/Modals/WorkspaceInviteModal.jsx
+++ src/components/organisms/Modals/WorkspaceInviteModal.jsx
@@ -51,7 +51,7 @@ export const WorkspaceInviteModal = ({ openInviteModal, setOpenInviteModal, work
                         {joinCode}
                     </p>
                     <Button size="sm" variant="ghost" onClick={handleCopy}>
-                        Copy Link
+                        Copy Code
                         <CopyIcon className='size-4 ml-2' />
                     </Button>
 
--- src/pages/Workspace/JoinPage.jsx
+++ src/pages/Workspace/JoinPage.jsx
@@ -1,14 +1,32 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import VerificationInput from 'react-verification-input';
 
 import { Button } from '@/components/ui/button';
+import { useJoinWorkspaceRequest } from '@/hooks/apis/workspaces/useJoinWorkspace';
+import { useToast } from '@/hooks/use-toast';
 
 export const JoinPage = () => {
 
     const { workspaceId } = useParams();
+    const navigate = useNavigate();
+    const { toast } = useToast();
 
-    async function handleAddMemberToWorkspace() {
-        console.log('Adding member to workspace');
+    const { joinWorkspaceMutation } = useJoinWorkspaceRequest(workspaceId);
+
+    async function handleAddMemberToWorkspace(joinCode) {
+        console.log('Adding member to workspace', joinCode);
+        try {
+            await joinWorkspaceMutation(joinCode);
+            toast({
+                title: 'You have been added to workspace successfully',
+                type: 'success'
+            });
+
+            navigate(`/workspaces/${workspaceId}`);
+
+        } catch(error) {
+            console.log('Error in adding member to workspace', error);
+        }
     }
 
     return (
